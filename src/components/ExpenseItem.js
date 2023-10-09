028b1e89c7270e6a751cd408fc0e7a3c6a90bc25
Niharika Goel <niharika.coer@gmail.com>
2023-10-09 20:51:27 +0530

    Practicing object destructuring of components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem({title, amount, date}) {
+
+  return (
+    <div className="expense-item">
+      <div>
+        <ExpenseDate date={date}></ExpenseDate>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
